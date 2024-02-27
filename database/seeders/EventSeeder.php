4bea167b04da4edf7efdf32cee6c92980b090467
Alex-fch <alexandre.foucher@live.fr>
2024-02-27 11:23:44 +0100

    Création de la table Event

---
+++ database/seeders/EventSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Event;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class EventSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\Event::factory(50)->create();
+    }
+}
